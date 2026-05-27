b8342ec6b660db164d0b59719938ca20b4a11915
Peilin_Wu <william20000904@gmail.com>
2026-05-27 17:59:38 +1200

    this update is for adding field, properties and constructor in the User class

--- User.cs
+++ User.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace FinalProject_ChunHoChoy_PeilinWu
 {
-    internal class User
+    public class User
     {
+        //fields for user details
+        //this is the parent class for all the users
+        //including the admin and the customer
+        private string username;
+        private string password;
+        private string role;
+
+        //properties for the fields
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        
+
+
+        //constructor for the user class
+        public User(string username, string password, string role)
+        {
+            Username = username;
+            Password = password;
+            Role = role;
+
+        }
     }
 }
