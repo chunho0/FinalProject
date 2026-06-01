c18a4e45408bedf96aa2a28c6b5120e95f2c2ecc
Peilin_Wu <william20000904@gmail.com>
2026-06-01 16:55:12 +1200

    none

--- Program.cs
+++ Program.cs
@@ -699,13 +699,16 @@ namespace FinalProject_ChunHoChoy_PeilinWu
 
                         Guest newCustomer = Guest.AddGuest("guest","null");
                         foreach (User user in users)
+                        { 
+                            if (user.Username.ToLower() == newCustomer.Username.ToLower())
+                            {
+                                Console.WriteLine("\nUsername already exists...");
+                                return;
+                            }
+                        }
                         users.Add(newCustomer);
                         
-                        if (newCustomer.Username == users. )
-                        {
-                            Console.WriteLine("You already have an account.");
-                        }
-                        Console.WriteLine("You have create a new account. ");
+                        
 
 
                         break;
